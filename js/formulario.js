0cd5259ae58735d7f9e8bde13ae3acf307694c01
MaCo07 <colquematias86@gmail.com>
2024-05-16 23:11:17 -0300

    Ultimos cambios

--- js/formulario.js
+++ js/formulario.js
@@ -3,11 +3,52 @@ const input = document.querySelectorAll('#formulario input');
 
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
-const validarFormulario = () => {
-    
+const campos = {
+    nombre: false,
+    email: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            validarCampo(expresiones.nombre, e.target, 'nombre');
+            // if(expresiones.nombre.test(e.target.value)){
+            //     document.getElementById('grupo__nombre').classList.remove('form_input__grupo-incorrecto');
+            //     document.getElementById('grupo__nombre').classList.add('form_input__grupo-correcto');
+            //     document.querySelector('#grupo__nombre .form__input-error').classList.remove('form__input-error-activo')
+
+            // } else{
+            //     document.getElementById('grupo__nombre').classList.add('form_input__grupo-incorrecto');
+            //     document.getElementById('grupo__nombre').classList.remove('form_input__grupo-correcto');
+            //     document.querySelector('#grupo__nombre .form__input-error').classList.add('form__input-error-activo');
+            // }
+        break;    
+        case "email":
+            validarCampo(expresiones.email, e.target, 'email')
+        break;    
+        case "mensaje":
+
+        break;    
+  
+    }
+}
+
+
+const validarCampo = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        document.getElementById(`grupo__${campo}`).classList.remove('form_input__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('form_input__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .form__input-error`).classList.remove('form__input-error-activo')
+        campos[campo] = true
+    } else{
+        document.getElementById(`grupo__${campo}`).classList.add('form_input__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('form_input__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .form__input-error`).classList.add('form__input-error-activo');
+        campos[campo] = false
+    }
 }
 
 input.forEach((input) => {
@@ -17,4 +58,10 @@ input.forEach((input) => {
 
 formulario.addEventListener('submit',(e) =>{
     e.preventDefault();
-});
+
+    const terminos = document.getElementById('terminos')
+    if(campos.nombre && campos.email && terminos.cheked){
+        formulario.reset();
+
+    }
+})
